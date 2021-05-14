398f42d4a4d85f8774c18a5226fc8299a4b1c1bb
Danilo Lustosa <danilolustosa@gmail.com>
2021-05-13 22:35:40 -0300

    first commit

---
+++ Repository/PessoaRepository.cs
@@ -0,0 +1,41 @@
+﻿using AquiCovidAPI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using AquiCovidAPI.Interface;
+
+namespace AquiCovidAPI.Repository
+{
+    public class PessoaRepository : IPessoaRepository
+    {
+        public string _connectionString = "Server=DANILO\\SQLEXPRESS;Database=AquiCovid;Trusted_Connection=True;";
+
+        public List<Pessoa> Listar()
+        {
+            string query = @"SELECT [Id]
+                                  ,[Nome]
+                                  ,[CPF]
+                              FROM [dbo].[Pessoa]";
+
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn.Query<Pessoa>(query).ToList();          
+        }
+
+        public Pessoa Obter(int Id)
+        {
+            string query = @"SELECT [Id]
+                                  ,[Nome]
+                                  ,[CPF]
+                              FROM [dbo].[Pessoa]
+                              WHERE [Id] = @Id";
+
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn.QueryFirstOrDefault<Pessoa>(query, new { Id });
+        }
+    }
+}
---
+++ Service/PessoaService.cs
@@ -0,0 +1,50 @@
+﻿using AquiCovidAPI.Entity;
+using AquiCovidAPI.Interface;
+using AquiCovidAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquiCovidAPI.Service
+{
+    public class PessoaService : IPessoaService
+    {
+        private IPessoaRepository _pessoaRepository;
+
+        public PessoaService(IPessoaRepository pessoaRepository)
+        {
+            _pessoaRepository = pessoaRepository;
+        }
+
+        public List<PessoaResponse> Listar()
+        {
+            var entity = _pessoaRepository.Listar();
+
+            List<PessoaResponse> response = new List<PessoaResponse>();
+
+            entity.ForEach(p => {
+                response.Add(new PessoaResponse() { 
+                    Id = p.Id,
+                    Nome = p.Nome,
+                    CPF = p.CPF
+                });
+            });
+
+            return response;            
+        }
+
+        public PessoaResponse Obter(int id)
+        {
+            var entity = _pessoaRepository.Obter(id);
+
+            return new PessoaResponse()
+            {
+                Id = entity.Id,
+                Nome = entity.Nome,
+                CPF = entity.CPF
+            };
+
+        }
+    }
+}
